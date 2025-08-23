95ff168c4407106f28b5926dd3049c0d90f31700
Boomika2006 <boomikakannan06@gmail.com>
2025-08-23 22:16:42 +0530

    Add files via upload

---
+++ Endoffile.c
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int c=1;
+        while(scan.hasNext())
+        {
+            String s=scan.nextLine();
+            System.out.println(c+" "+s);
+            c++;
+        }
+    }
+}
